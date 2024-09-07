3a9b418a8129c36d1162f8d500a0d91b10d0acba
けん <58802704+hiraiwa0928@users.noreply.github.com>
2024-09-07 18:09:09 +0900

    APIでhello worldが出るようにする

--- api/src/main.go
+++ api/src/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	// Ginエンジンのインスタンスを作成
+	r := gin.Default()
+
+	// ルートURL ("/") に対するGETリクエストをハンドル
+	r.GET("/", func(c *gin.Context) {
+		// JSONレスポンスを返す
+		c.JSON(200, gin.H{
+			"message": "Hello World",
+		})
+	})
+
+	// 8080ポートでサーバーを起動
+	r.Run(":8080")
 }
