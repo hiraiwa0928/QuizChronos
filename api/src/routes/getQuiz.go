8fadaa9d7d1ffff52338822b38b46e8a5e09b663
けん <58802704+hiraiwa0928@users.noreply.github.com>
2024-09-28 17:48:44 +0900

    POSTをGETに変更

--- api/src/routes/getQuiz.go
+++ api/src/routes/getQuiz.go
@@ -3,7 +3,7 @@ package routes
 import "github.com/gin-gonic/gin"
 
 func SetupQuizRoutes(r *gin.Engine) {
-	r.POST("/quiz", func(c *gin.Context) {
+	r.GET("/quiz", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"quiz-id": 1,
 			"prob-id": 1,
