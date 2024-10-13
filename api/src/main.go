195b148a680353c08aa74007dd893df2863bd442
けん <58802704+hiraiwa0928@users.noreply.github.com>
2024-10-13 22:31:42 +0900

    修正

--- api/src/main.go
+++ api/src/main.go
@@ -9,7 +9,7 @@ func main() {
 	// Ginエンジンのインスタンスを作成
 	r := gin.Default()
 
-	routes.SetupQuizRoutes(r)
+	routes.SetupRoutes(r)
 
 	// 8080ポートでサーバーを起動
 	r.Run(":8080")
---
+++ api/src/service/getQuiz.go
@@ -0,0 +1,10 @@
+package service
+
+import "github.com/gin-gonic/gin"
+
+func GetQuiz(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"quiz-id": 100,
+		"prob-id": 1323233,
+	})
+}
